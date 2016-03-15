21cc1868854691187648fb515df4d950b4a9a40c
vtoudic <vtoudic@B4-W7-15.sio.chaptalqper>
2016-03-15 16:34:42 +0100

    modif

--- application/views/v-connexion.php
+++ application/views/v-connexion.php
@@ -1,32 +1,56 @@
-
-<!-- Bootstrap -->
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+  
+  <!-- Bootstrap -->
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
- 
+
+	<img src="images/logo.jpg"  class="img-circle">
+	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <title>Connexion</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/theme.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/signin.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Identifiez-vous !</h2>
+        <input type="text" class="form-control" placeholder="Pseudo" autofocus>
+        <input type="password" class="form-control" placeholder="Password">
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+      </form>
+
+    </div> <!-- /container -->
 
 
-<div id="contenu">
-	<h2>Identification utilisateur</h2>
-
-
-	<form method="POST" action="">
-		<p>
-			<label for="login">Login*</label>
-			<input id="login" type="text" name="login"  size="30" maxlength="45"/>
-		</p>
-		<p>
-			<label for="nom">Mot de passe*</label>
-			<input id="mdp"  type="password"  name="mdp" size="30" maxlength="45"/>
-		</p>
-		 
-		<p>
-			<input type="submit" value="Valider" name="valider"/>
-			<input type="reset" value="Annuler" name="annuler"/> 
-		</p>
-	</form>
-
-	<img src="images/logo.jpg" />
-</div>
-
-
-<script src="assets/js/bootstrap.min.js"></script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+  
+  <script src="assets/js/bootstrap.min.js"></script>
  <script src="js/bootstrap.min.js"></script>
+ 
+ 
+</html>
+ 
