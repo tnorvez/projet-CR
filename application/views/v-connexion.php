d528450588ab81555352e1954c53c54ab4c93cfc
vtoudic <vtoudic@B4-W7-15.sio.chaptalqper>
2016-03-15 15:27:05 +0100

    pour teddy

---
+++ application/views/v-connexion.php
@@ -0,0 +1,32 @@
+
+<!-- Bootstrap -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 
+
+
+<div id="contenu">
+	<h2>Identification utilisateur</h2>
+
+
+	<form method="POST" action="">
+		<p>
+			<label for="login">Login*</label>
+			<input id="login" type="text" name="login"  size="30" maxlength="45"/>
+		</p>
+		<p>
+			<label for="nom">Mot de passe*</label>
+			<input id="mdp"  type="password"  name="mdp" size="30" maxlength="45"/>
+		</p>
+		 
+		<p>
+			<input type="submit" value="Valider" name="valider"/>
+			<input type="reset" value="Annuler" name="annuler"/> 
+		</p>
+	</form>
+
+	<img src="images/logo.jpg" />
+</div>
+
+
+<script src="assets/js/bootstrap.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
